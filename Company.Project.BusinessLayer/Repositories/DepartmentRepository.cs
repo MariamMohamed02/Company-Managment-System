8e4b8e2627c069c183733435ddb20d024e647441
MariamMohamed02 <mariamsamy811@gmail.com>
2025-03-19 00:19:12 +0200

    Department Module BLL

---
+++ Company.Project.BusinessLayer/Repositories/DepartmentRepository.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Company.Project.BusinessLayer.Interfaces;
+using Company.Project.DataLayer.Data.Contexts;
+using Company.Project.DataLayer.Models;
+
+namespace Company.Project.BusinessLayer.Repositories
+{
+    public class DepartmentRepository : IDepartmentRepository
+    {
+        private readonly CompanyDbContext _context;
+        public DepartmentRepository()
+        {
+            _context = new CompanyDbContext();
+        }
+        public int Add(Department model)
+        {
+
+            _context.Departments.Add(model);
+            return _context.SaveChanges();
+        }
+
+        public int Delete(Department model)
+        {
+            _context.Departments.Remove(model);
+            return _context.SaveChanges();
+        }
+
+        public Department? Get(int id)
+        {
+
+            return _context.Departments.Find(id);
+        }
+
+        public IEnumerable<Department> GetAll()
+        {
+            return _context.Departments.ToList();
+        }
+
+        public int Update(Department model)
+        {
+            _context.Departments.Update(model);
+            return _context.SaveChanges();
+        }
+    }
+}
