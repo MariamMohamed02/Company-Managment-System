3dcbf5ec16c087d6d3356927cad67aa1f10b361a
MariamMohamed02 <139442036+MariamMohamed02@users.noreply.github.com>
2025-04-13 11:41:35 +0200

    Implemented DAL for Employee Service

--- Company.Project.DataLayer/Data/Contexts/CompanyDbContext.cs
+++ Company.Project.DataLayer/Data/Contexts/CompanyDbContext.cs
@@ -27,6 +27,7 @@ namespace Company.Project.DataLayer.Data.Contexts
         //}
 
         public DbSet<Department> Departments { get; set; }
+        public DbSet<Employee> Employees { get; set; }
 
     }
 
