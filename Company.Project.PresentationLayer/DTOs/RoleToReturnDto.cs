d0650db07dc7834e9fc60bd0fd3174e02bf03688
MariamMohamed02 <139442036+MariamMohamed02@users.noreply.github.com>
2025-04-21 17:33:00 +0200

    Implemented Role Controller

---
+++ Company.Project.PresentationLayer/DTOs/RoleToReturnDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Company.Project.PresentationLayer.DTOs
+{
+    public class RoleToReturnDto
+    {
+        public string? Id { get; set; }
+        public string Name { get; set; }
+    }
+}
